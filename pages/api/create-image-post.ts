3efb6ada5951012cca76827f87cf423271154913
jason-evangelista <evangelistajasonglenn@gmail.com>
2022-10-31 23:42:36 +0800

    ⚡️ Add upload image post api route

---
+++ pages/api/create-image-post.ts
@@ -0,0 +1,46 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import verifyUserSession from "@utils/api/verifyUserSession";
+import UploadImageForm from "@components/UploadImagePage/type/upload.image.form";
+
+type OverrideBodyReq = UploadImageForm & {
+  imagePath: string;
+};
+
+const EXPIRE_IN = 240 * 3600;
+
+const uploadImage = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { user, supabaseServer, error } = await verifyUserSession(req, res);
+
+  if (!user) return res.status(401).json({ message: "Unauthorized" });
+  if (error) return res.status(400).json({ error });
+
+  if (req.method !== "POST")
+    return res
+      .status(400)
+      .json({ message: "This api call is only available for POST" });
+  try {
+    const bodyPayload = req.body as OverrideBodyReq;
+    console.log(bodyPayload);
+    const { data, error: signImgError } = await supabaseServer.storage
+      .from("images")
+      .createSignedUrl(bodyPayload.imagePath, EXPIRE_IN);
+    console.log({ signImgError, data });
+
+    const { error } = await supabaseServer.rpc("insert_image_post", {
+      image_url: data?.signedUrl || "",
+      description: bodyPayload.description,
+      compress_action: bodyPayload.compressAction,
+      created_by: user.id,
+      is_public: bodyPayload.isPublic,
+    });
+
+    if (error) return res.status(400).json({ message: error.message });
+    return res
+      .status(200)
+      .json({ message: "Successfully insert", redirectUrl: "/p/dashboard" });
+  } catch (e) {}
+
+  return res.status(200).json({});
+};
+
+export default uploadImage;
